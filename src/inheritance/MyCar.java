bafe033600ab779d7339df2439a9c647c2727c66
Sam Lemon <sam262417@gmail.com>
2018-09-15 17:33:49 -0500

    commit mycar.java

--- src/inheritance/MyCar.java
+++ src/inheritance/MyCar.java
@@ -5,16 +5,6 @@ public class MyCar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	/*	HondaCivic[] myCar = new HondaCivic[3];
-		myCar[0] = new HondaCivic("HondaCivic","Sedan");
-		myCar[1] = new HondaCivic("HondaCivic","Hatchback");
-		myCar[2] = new HondaCivic("HondaCivic","SUV");
-		for (HondaCivic hc:myCar)
-		{
-			System.out.println(hc.getModel() + " " + hc.getType());
-		}
-		*/
-	
 		Car[] myCar = new Car[3];
 		myCar[0] = new Car("HondaCivic","Sedan");
 		myCar[1] = new Car("HondaCivic","Hatchback");
@@ -24,7 +14,6 @@ public class MyCar {
 			System.out.println(hc.getModel() + " " + hc.getType());
 		}
 		
-	
 	}
 
 }
