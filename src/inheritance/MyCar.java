cb3b77119116d4fa2e9822d74ffdd31e6b3096ef
Acharya <madhava.acharya@capgemini.com>
2018-09-15 21:35:54 -0500

    capgemini MyCar.java changes

--- src/inheritance/MyCar.java
+++ src/inheritance/MyCar.java
@@ -5,10 +5,11 @@ public class MyCar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Car[] myCar = new Car[3];
-		myCar[0] = new Car("HondaCivic","Sedan");
-		myCar[1] = new Car("HondaCivic","Hatchback");
-		myCar[2] = new Car("HondaCivic","SUV");
+		Car[] myCar = new Car[4];
+		myCar[0] = new Car("HondaAccord","Car");
+		myCar[1] = new Car("HondaCivic","Small Car");
+		myCar[2] = new Car("HondaCRV","SUV");
+		myCar[3] = new Car("HondaOdyssy","Mini Van");
 		for (Car hc:myCar)
 		{
 			System.out.println(hc.getModel() + " " + hc.getType());
